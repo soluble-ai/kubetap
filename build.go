a0b7f19433fd5693847af050b4308da51352b031
Matt Hamilton <matt@soluble.ai>
2020-09-29 02:12:36 -0400

    go generate

---
+++ build.go
@@ -0,0 +1,10 @@
+//go:generate ./scripts/deps.sh
+//go:generate go clean -i ./...
+//go:generate rm -f ./cmd/kubectl-tap/kubectl-tap
+//go:generate go mod download
+//go:generate golangci-lint run
+//go:generate gotestsum --format=short-verbose --no-summary=skipped --junitfile=coverage.xml -- -count=1 -race -coverprofile=coverage.txt -covermode=atomic ./...
+//go:generate ./scripts/ig-test.zsh
+package main
+
+func main() {}
